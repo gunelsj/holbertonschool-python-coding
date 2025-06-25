89ba0d772ea33a9a6cbbc4988779db9ed20633f0
gunelsj <gunelsj@code.edu.az>
2025-06-25 07:15:21 +0000

    Fix: add docstrings and private size attribute

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""This module defines a class Square"""
+
 class Square:
+    """This class defines a square with private size"""
     def __init__(self, size):
         self.__size = size
